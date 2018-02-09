44c1c3ed7b294dc5b6f42bedee33b4205271e611
LION-HACK <33150334+lnwseed@users.noreply.github.com>
2018-02-09 11:11:01 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,47 @@
+	cd /usr/local/bin
+	rm -f bench-network
+	rm -f menu
+	rm -f ram
+  rm -f speedtest
+  rm -f user-add
+  rm -f user-addday
+	rm -f user-ban
+	rm -f user-delete
+  rm -f user-generate
+	rm -f user-limit
+  rm -f user-list
+  rm -f user-password
+	rm -f user-trial
+	rm -f user-unban
+  
+	wget http://xn--l3clxf6cwbe0gd7j.com/ocspanel/menufree/bench-network
+  wget http://xn--l3clxf6cwbe0gd7j.com/ocspanel/menufree/menu
+  wget http://xn--l3clxf6cwbe0gd7j.com/ocspanel/menufree/ram
+  wget http://xn--l3clxf6cwbe0gd7j.com/ocspanel/menufree/speedtest
+  wget http://xn--l3clxf6cwbe0gd7j.com/ocspanel/menufree/user-add
+  wget http://xn--l3clxf6cwbe0gd7j.com/ocspanel/menufree/user-addday
+  wget http://xn--l3clxf6cwbe0gd7j.com/ocspanel/menufree/user-ban
+  wget http://xn--l3clxf6cwbe0gd7j.com/ocspanel/menufree/user-delete
+  wget http://xn--l3clxf6cwbe0gd7j.com/ocspanel/menufree/user-generate
+  wget http://xn--l3clxf6cwbe0gd7j.com/ocspanel/menufree/user-limit
+  wget http://xn--l3clxf6cwbe0gd7j.com/ocspanel/menufree/user-list
+  wget http://xn--l3clxf6cwbe0gd7j.com/ocspanel/menufree/user-password
+  wget http://xn--l3clxf6cwbe0gd7j.com/ocspanel/menufree/user-trial
+  wget http://xn--l3clxf6cwbe0gd7j.com/ocspanel/menufree/user-unban     
+  
+  chmod +x bench-network
+  chmod +x menu
+  chmod +x ram
+  chmod +x speedtest
+  chmod +x user-add
+  chmod +x user-addday
+  chmod +x user-ban
+  chmod +x user-delete
+  chmod +x user-generate
+  chmod +x user-limit
+  chmod +x user-list
+  chmod +x user-password
+  chmod +x user-trial
+  chmod +x user-unban
+	cd
+  
